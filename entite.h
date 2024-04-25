f7a0dc801bbb6da1617105016b953102986527a5
fatoumet <166331296+fatoumet@users.noreply.github.com>
2024-04-25 07:53:19 +0100

    Create entite.h

---
+++ entite.h
@@ -0,0 +1,39 @@
+#ifndef entite_H_INCLUDED
+#define entite_H_INCLUDED
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+
+
+typedef struct{
+    SDL_Surface *img[2][2];
+    int direction;
+    SDL_Rect pos;
+    int anim;
+}entite;
+
+/*typedef struct{
+    int mu;
+    int md;
+    int ml;
+    int mr;
+} direction;*/
+
+
+void initEnty(entite *e);
+void initEnty2(entite *e2);
+void afficherEnnemi(entite e,SDL_Surface* screen);
+void afficherEnnemi2(entite e2,SDL_Surface* screen);
+void animerEntity(entite *e);
+void animerEntity2(entite *e2);
+int collisionTri(entite e,SDL_Rect pos);
+void move(entite *e);
+void move2(entite *e2);
+int collisionBB(entite e,SDL_Rect pos);
+//void moveIA(entite *e);  
+
+
+#endif
